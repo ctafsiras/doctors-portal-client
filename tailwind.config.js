48b94223f540797210953f46f2f2255b065b8b37
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2022-05-10 21:57:20 +0600

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,21 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        doctorstheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#19D3AE",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      // "dark",
+      "cupcake",
+    ],
+  },
+
 
 }
